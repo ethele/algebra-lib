4769c93aca7c8bab58e23e0214c7ca4bac8e23fb
Lenora Ellison <Lenora.brigid@gmail.com>
2019-01-06 14:56:00 -0800

    Adds beginnings of expression node trees but does not use yet

--- algebra/algebra.py
+++ algebra/algebra.py
@@ -4,4 +4,39 @@ class Algebra:
         return
 
     def simplify(self, expression):
-        return expression
+        builder = ExpressionTreeBuilder()
+        builder.expression = expression
+        expression_tree = builder.build_tree()
+        return str(expression_tree)
+
+class ExpressionTreeBuilder:
+
+    def __init__(self):
+        self.expression = ""
+        self.root = None
+
+    def build_tree(self):
+        binary_operators = "+"
+        nodes_list = str.split(self.expression)
+        print (nodes_list)
+        for node in nodes_list:
+            if self.root is None:
+                print("Making new root")
+                #self.root = ExpressionNode(node)
+            elif node in binary_operators:
+                print("Making new binary node")
+        self.root = ExpressionNode(self.expression)
+        return self.root
+
+    def get_simplified_tree(self):
+        return self.root
+
+# class ExpressionTree:
+
+class ExpressionNode:
+
+    def __init__(self, expression):
+        self.value = expression
+
+    def __str__(self):
+        return self.value
--- algebra_test.py
+++ algebra_test.py
@@ -7,3 +7,16 @@ def test_simplify_3():
     solver = algebra.Algebra()
     answer = solver.simplify("3")
     assert answer == "3"
+
+# Next steps: Create interface ExpressionNode
+# Create implementing classes ValueNode / UnaryExpressionNode 
+# and BinaryExpressionNode
+# Then when see binary operator, create new BinaryExpressionNode
+# With 'root' on the left and a new expression tree (root node) to the right 
+# generated from the remainder of the expression
+# with a stored operator value for the node
+
+# def test_add_two_digits():
+#     solver = algebra.Algebra()
+#     answer = solver.simplify("3 + 4")
+#     assert answer == "7"
